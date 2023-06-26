14afc02975ffc7d56a6d75d141d83dc0c9b5eae2
akio0911 <akio0911@gmail.com>
2023-06-26 19:31:24 +0900

    feedback

--- HinakkoKadai10/HinakkoKadai10/ViewController.swift
+++ HinakkoKadai10/HinakkoKadai10/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     let data = PrefectureData()
 
+    private let colors: [UIColor] = [
+        UIColor(red: 255/255, green: 100/255, blue: 100/255, alpha: 100/100),
+        UIColor(red: 100/255, green: 255/255, blue: 100/255, alpha: 100/100),
+        UIColor(red: 100/255, green: 100/255, blue: 255/255, alpha: 100/100)
+    ]
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         data.prefecturesList.count
     }
@@ -20,14 +26,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         cell.prefectureLabel?.text = data.prefecturesList[indexPath.row]
         cell.numberLabel?.text = "\(indexPath.row + 1)番目の都道府県です"
 
-        switch indexPath.row % 3 {
-        case 0:
-            cell.backgroundColor = UIColor.init(red: 255/255, green: 100/255, blue: 100/255, alpha: 100/100)
-        case 1:
-            cell.backgroundColor = UIColor.init(red: 100/255, green: 255/255, blue: 100/255, alpha: 100/100)
-        default:
-            cell.backgroundColor = UIColor.init(red: 100/255, green: 100/255, blue: 255/255, alpha: 100/100)
-        }
+        cell.backgroundColor = colors[indexPath.row % colors.count]
+
         return cell
     }
 }
